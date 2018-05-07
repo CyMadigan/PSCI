47ff65999d21ae809351b3ffb76ae86e14477008
Krzysztof Najda <knajda@objectivity.co.uk>
2018-05-07 18:25:41 +0200

    Updated license file header

--- build/sanity_checks.ps1
+++ build/sanity_checks.ps1
@@ -1,7 +1,7 @@
 <#
 The MIT License (MIT)
 
-Copyright (c) 2015 Objectivity Bespoke Software Specialists
+Copyright (c) 2018 Objectivity Bespoke Software Specialists
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
